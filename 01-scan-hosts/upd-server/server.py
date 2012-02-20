c6a98e4cc16cc975c418213b1bdcd8f2ed777a46
Jonathan Stoppani <jonathan@stoppani.name>
2012-02-20 13:23:40 +0100

    Added an inline help to the server script and it is now possible to pass in a port number when starting the server.

--- 01-scan-hosts/upd-server/server.py
+++ 01-scan-hosts/upd-server/server.py
@@ -1,3 +1,14 @@
+"""
+You can run this file by executing:
+
+    python server.py 1024
+
+on the command line. The twisted networking library is
+required in order to run the server.
+"""
+
+import sys
+
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
@@ -7,5 +18,5 @@ class Echo(DatagramProtocol):
         print "received %r from %s:%d" % (data, host, port)
         self.transport.write(data, (host, port))
 
-reactor.listenUDP(1024, Echo())
+reactor.listenUDP(int(sys.argv[1]), Echo())
 reactor.run()
